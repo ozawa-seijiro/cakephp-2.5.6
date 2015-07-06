8bb66dc69e41c52c343458fab2e9cff869705bef
ozawaseijiro <seijiro4751@gmail.com>
2015-07-06 17:03:01 +0900

    remove enviroment

--- app/Config/bootstrap/environments.php
+++ app/Config/bootstrap/environments.php
@@ -3,6 +3,6 @@ CakePlugin::load('Environments');
 App::uses('Environment', 'Environments.Lib');
  
 include dirname(__FILE__) . DS . 'environments' . DS . 'heroku.php';
-include dirname(__FILE__) . DS . 'environments' . DS . 'development.php';
+# include dirname(__FILE__) . DS . 'environments' . DS . 'development.php';
  
 Environment::start();
